8c8d03895486c470a3e8051f5975d1a7c5b17ed5
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-08-08 09:23:04 -0700

    add question data and eval script

---
+++ my_eval_script.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+python fastchat/llm_judge/gen_model_answer.py --model-path /home/lidong1/jianglingjie/LLama-Factory/model_checkpoint/huggingface/Meta-Llama-3.1-8B --model-id llama3.1-8b-base
+
+python gen_model_answer.py --model-path /home/lidong1/jianglingjie/LLama-Factory/model_checkpoint/huggingface/Meta-Llama-3.1-8B --model-id llama3.1-8b-base
