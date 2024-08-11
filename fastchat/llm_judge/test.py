533fb175c05bd6963e539d5e857d3c18e9af8e9b
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2024-08-11 08:44:48 -0700

    add chat template for llama3.1 base

---
+++ fastchat/llm_judge/test.py
@@ -0,0 +1,15 @@
+from openai import OpenAI
+client = OpenAI(
+    base_url="http://localhost:8000/v1",
+    api_key="token-abc123",
+)
+
+completion = client.chat.completions.create(
+  # model="/home/lidong1/jianglingjie/LLama-Factory/model_checkpoint/huggingface/Meta-Llama-3.1-8B-Instruct",
+  model="/home/lidong1/jianglingjie/LLama-Factory/model_checkpoint/huggingface/Meta-Llama-3.1-8B",
+  messages=[
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
